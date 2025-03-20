8279615afbc440093e7a5b49e10d995d7844d916
Md Rakibuzzaman Rakib <rakibzaman724@gmail.com>
2025-03-20 10:18:39 +0200

    update mongomoviedb

--- mongomoviedb/server.js
+++ mongomoviedb/server.js
@@ -7,12 +7,39 @@ const app = express();
 app.use(express.json());
 app.use("/", routes);
 
+
 //MongoDB connection
-dotenv.config({path: './movie.env'});
+dotenv.config({path: "./.env"});
 const mongoURL = process.env.MONGO_URI;
-mongoose.connect(mongoURL, { dbName: 'moviedb', useNewUrlParser: true , useUnifiedTopology: true});
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error'));
+
+const connectTodb = async () => {
+  try {
+    await mongoose.connect(mongoURL, {
+      dbName: 'cardb',
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log("Mongodb connected successfully.");
+  } catch (err) {
+    console.error("MOngodb connection error", err);
+    process.exit(1);
+  }
+}
+connectTodb();
+
+mongoose.connection.on('error', (err) => {
+  console.error("MongoDB connectioin error",err);
+});
+
+mongoose.connection.once('open', () => {
+  console.log("MongoDB connection established");
+});
+
+// dotenv.config({path: './.env'});
+// const mongoURL = process.env.MONGO_URI;
+// mongoose.connect(mongoURL, { dbName: 'moviedb', useNewUrlParser: true , useUnifiedTopology: true});
+// const db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'MongoDB connection error'));
 
 
 const port = 3000;
